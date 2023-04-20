4a3798d73952821cb0c3ba3177c7973b4c485af9
maars0hot <127972939+maars0hot@users.noreply.github.com>
2023-04-20 22:45:22 +0600

    Complete HW

---
+++ HW66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+//Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите целое положительное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое положительное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < numberN) 
+{
+    Console.WriteLine($"{SumNumbers(numberM, numberN)}");
+}
+else Console.WriteLine($"{SumNumbers(numberN, numberM)}");
+
+
+
+int SumNumbers(int M, int N)
+{
+    if (N + 1 == M) return 0;
+    return M + SumNumbers(M + 1, N);
+}
---
+++ HW68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите целое положительное число n:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое положительное число m:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int result = Levi(numberN,numberM);
+Console.WriteLine($"A({numberN},{numberM}) = {result}");
+
+int Levi(int n,int m)
+{
+   if (n==0) return m+1;
+   if (m == 0) return Levi(n-1,1);
+   return Levi (n-1,Levi(n,m-1));
+}
